5ae3f0e5bfb0e37a82fd9b6977d7521a1af7862d
RogerJackD <rogerzapata114@gmail.com>
2025-07-31 20:37:31 -0500

    feat(files): add upload and retrieval endpoints for product images

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,28 +1,56 @@
-import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+import { fileFilter, fileNamer } from './helpers';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ){}
 
- @Post('product')
- @UseInterceptors( FileInterceptor('file',{
-  fileFilter: fileFilter
- }) )
- uploadProductImage(
-  @UploadedFile() file: Express.Multer.File 
-){
-  
-
-  if( !file ){
-    throw new BadRequestException('make sure that the file i an  image')
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  ){
+    const path = this.filesService.getStaticProductImage( imageName );
+    res.sendFile( path );
   }
 
-  return {
-    filename: file.originalname
-  };
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      //limits: { fileSize: 1000 }
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('make sure that the file i an  image');
+    }
 
- }
+    const secureUrl = `${ this.configService.get('HOST_API') }/files/product/${ file.filename }`;
+
+    return {
+      secureUrl,
+    };
+  }
 }
