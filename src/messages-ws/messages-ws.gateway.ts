ccd1e8bf645ce70ed35eb4c97c45406f31e6b478
RogerJackD <rogerzapata114@gmail.com>
2025-08-09 23:59:11 -0500

    feat(messages): emit clients-updated event on connect and disconnect

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,22 +1,27 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors : true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  
+  @WebSocketServer() wss : Server
+
+  constructor(
+    private readonly messagesWsService: MessagesWsService
+  ) {}
 
 
   handleConnection(client: Socket ) {
-    //console.log(client)
-    //console.log('cliente conectado', client.id);
     this.messagesWsService.registerClient( client );
-    console.log({ conectados : this.messagesWsService.getConnectedClients() });
-
+    
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients() );
   }
   handleDisconnect(client: Socket ) {
     //console.log('cliente desconectado', client.id);
     this.messagesWsService.removeClient( client.id );
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients() );
+
   }
 
 
