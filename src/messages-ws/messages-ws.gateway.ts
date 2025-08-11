439c9d923397963eaaeb1f8dc3a10eb880d9d36e
RogerJackD <rogerzapata114@gmail.com>
2025-08-10 23:40:31 -0500

    refactor(messages-gateway): validate JWT on connection and broadcast authenticated events

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,6 +1,10 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
+import { subscribe } from 'diagnostics_channel';
+import { NewMessageDto } from './dto/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces';
 
 @WebSocketGateway({ cors : true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -8,15 +12,30 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   @WebSocketServer() wss : Server
 
   constructor(
-    private readonly messagesWsService: MessagesWsService
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
   ) {}
 
 
-  handleConnection(client: Socket ) {
-    this.messagesWsService.registerClient( client );
-    
+  async handleConnection(client: Socket ) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+
+    try {
+
+      payload = this.jwtService.verify( token );
+      await this.messagesWsService.registerClient( client, payload.id );
+
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    //console.log({payload})
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients() );
   }
+
+
   handleDisconnect(client: Socket ) {
     //console.log('cliente desconectado', client.id);
     this.messagesWsService.removeClient( client.id );
@@ -24,6 +43,30 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
 
   }
 
+
+  @SubscribeMessage('message-from-client')
+  onleMessageFromClient( client: Socket, payload: NewMessageDto){
+
+    //!emite solo al cliente
+    // client.emit('message-from-server',{
+    //   fullName: 'soy me',
+    //   message: payload.message || 'NO-message'
+    // })
+    //!emite todos MENOS al cliente inicial
+    // client.broadcast.emit('message-from-server',{
+    //   fullName: 'soy me',
+    //   message: payload.message || 'NO-message'
+    // })
+
+
+    //!emite todos incluido el cliente inicial
+    this.wss.emit('message-from-server',{
+      fullName: this.messagesWsService.getUserFullName( client.id ),
+      message: payload.message || 'NO-message'
+    })
+
+
+  }
 
 
 
